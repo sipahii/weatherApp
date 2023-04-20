da3723477910acda573c60f733c5b736a528bf0f
shahanshah <mohommed.shanshah@upgrad.com>
2023-04-20 13:00:52 +0530

    final commit

---
+++ pages/weather/[zip].js
@@ -0,0 +1,96 @@
+import React from "react";
+import cities from "../../lib/city.list.json";
+import TodaysWeather from "../../components/TodaysWeather";
+import HourlyWeather from "../../components/HourlyWeather";
+import WeeklyWeather from "../../components/WeeklyWeather";
+import Link from "next/link";
+import Head from "next/head";
+import SearchBox from "../../components/SearchBox";
+import moment from "moment-timezone";
+
+const API_KEY="3050828d775a7c1de9a5bc06bf111c01"
+
+export async function getServerSideProps(context) {
+  const city = getCity(context.params.zip);
+
+  if (!city) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const res = await fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${city.coord.lat}&lon=${city.coord.lon}&appid=${API_KEY}&exclude=minutely&units=metric`
+  );
+  const data = await res.json();
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const hourlyWeather = getHourlyWeather(data.hourly, data.timezone);
+
+  const weeklyWeather = data.daily;
+
+  return {
+    props: {
+      city: city,
+      timezone: data.timezone,
+      currentWeather: data.current,
+      hourlyWeather: hourlyWeather,
+      weeklyWeather: weeklyWeather,
+    }, // will be passed to the page component as props
+  };
+}
+
+const getCity = (zip) => {
+  const city = cities.find((city) => city.id.toString() == zip);
+
+  if (city) {
+    return city;
+  } else {
+    return null;
+  }
+};
+
+const getHourlyWeather = (hourlyData, timezone) => {
+  const endOfDay = moment().tz(timezone).endOf("day").valueOf();
+  const eodTimeStamp = Math.floor(endOfDay / 1000);
+
+  const todaysData = hourlyData.filter((data) => data.dt < eodTimeStamp);
+
+  return todaysData;
+};
+
+export default function City({
+  city,
+  hourlyWeather,
+  weeklyWeather,
+  timezone,
+}) {
+  return (
+    <>
+      <Head>
+        <title>{city.name} Weather - Next Weather App</title>
+      </Head>
+
+      <div className="page-wrapper">
+        <div className="container">
+          <Link href="/">
+            <a className="back-link">&larr; Home</a>
+          </Link>
+          <SearchBox placeholder="Search for another location..." />
+          <TodaysWeather
+            city={city}
+            weather={weeklyWeather[0]}
+            timezone={timezone}
+          />
+          <HourlyWeather hourlyWeather={hourlyWeather} timezone={timezone} />
+          <WeeklyWeather weeklyWeather={weeklyWeather} timezone={timezone} />
+        </div>
+      </div>
+    </>
+  );
+}
